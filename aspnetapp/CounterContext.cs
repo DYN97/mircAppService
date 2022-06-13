c7ac9b856413c1373b825f9d8cca85eea98a6564
dyn <11236900984@qq.com>
2022-06-13 13:42:29 +0800

    [IMP] 修改数据库地址

--- aspnetapp/CounterContext.cs
+++ aspnetapp/CounterContext.cs
@@ -32,7 +32,10 @@ namespace aspnetapp
             {
                 var username = "root";
                 var password = "bSmR4pnC";
-                var connstr = $"server=sh-cynosdbmysql-grp-b798bq8o.sql.tencentcdb.com;port=26621;user={username};password={password};database=aspnet_demo";
+                var addressParts = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQL_ADDRESS"))? Environment.GetEnvironmentVariable("MYSQL_ADDRESS"): "sh-cynosdbmysql-grp-b798bq8o.sql.tencentcdb.com").Split(':');
+                var host = addressParts?[0];
+                var port = addressParts?[1]; 
+                var connstr = $"server={host};port={port};user={username};password={password};database=aspnet_demo";
                 optionsBuilder.UseMySql(connstr, Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.18-mysql"));
                 optionsBuilder.EnableSensitiveDataLogging();
                 optionsBuilder.LogTo(Console.WriteLine);
